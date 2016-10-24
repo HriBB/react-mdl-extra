ffbf48bf6b256be84f18d67f22343cb635fc32a6
Bojan Hribernik <bojan.hribernik@gmail.com>
2016-10-24 04:22:24 +0200

    Update menu story

--- stories/Menu.story.js
+++ stories/Menu.story.js
@@ -8,7 +8,7 @@ import { Menu, MenuItem } from '../src'
 
 storiesOf('Menu', module)
   .add('default', () => (
-    <Menu target={<Button raised>Open menu</Button>}>
+    <Menu target={<Button raised>Open menu</Button>} align={'tl bl'}>
       <MenuItem onClick={() => console.log('select one')}>One</MenuItem>
       <MenuItem onClick={() => console.log('select two')}>Two</MenuItem>
       <MenuItem onClick={() => console.log('select three')}>Three</MenuItem>
