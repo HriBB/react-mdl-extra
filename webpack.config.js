43911c2b3885d998951c3299c73c8a4e13a05af8
Bojan Hribernik <bojan.hribernik@gmail.com>
2016-11-22 09:10:20 +0100

    fix: Exclude react from UMD build.

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,11 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var env = process.env.NODE_ENV;
 var suffix = env === 'production' ? '.min' : '';
 var filename = `react-mdl-extra${suffix}`;
+var minify = process.env.MINIFY;
 
 var config = {
   entry: [
-    path.resolve(__dirname, 'src', 'index')
+    path.resolve(__dirname, 'src', 'index'),
   ],
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -20,23 +21,30 @@ var config = {
   resolve: {
     extensions: ['', '.js', '.scss', '.css'],
   },
+  externals: {
+    'react': 'react',
+    'react-dom' : 'reactDOM',
+  },
   module: {
     preLoaders: [{
       test: /\.js$/,
       loader: 'eslint',
-      exclude: /node_modules/
+      exclude: /node_modules/,
     }],
     loaders: [{
       test: /\.js$/,
       loader: 'babel',
-      exclude: /node_modules/
+      exclude: /node_modules/,
     },{
       test: /\.(scss|css)$/,
       loader: ExtractTextPlugin.extract('css!postcss!sass'),
     }]
   },
-  postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ],
+  postcss: [
+    autoprefixer({ browsers: ['last 2 versions'] })
+  ],
   plugins: [
+    new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(env)
@@ -44,5 +52,23 @@ var config = {
     new ExtractTextPlugin(`${filename}.css`)
   ],
 };
+
+if (minify) {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        screw_ie8: true,
+        warnings: false,
+      },
+      mangle: {
+        screw_ie8: true,
+      },
+      output: {
+        comments: false,
+        screw_ie8: true,
+      },
+    })
+  );
+}
 
 module.exports = config
